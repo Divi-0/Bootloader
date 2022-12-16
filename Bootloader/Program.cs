f4e743941f34a666ca5f8e1a746bbabffc5aae59
Divi-0 <84730274+Divi-0@users.noreply.github.com>
2022-12-16 10:05:58 +0100

    init commit

---
+++ Bootloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bootloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
